4a1e1788910b1b724719f4bc55d9a72f3eed7a83
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-17 19:00:52 -0700

    commit tree bb202779e2c805809a643338f1d5248a02ebea8a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const whatever = document.getElementById("text");
+    whatever.textContent = "This is really cool!";
+  });
